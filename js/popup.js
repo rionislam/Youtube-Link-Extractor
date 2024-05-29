ecf115f2cd8bf95f8d1976ee8b705fe95e0238e4
rionislam <rionislam.mazidul@gmail.com>
2024-05-29 20:13:32 +0600

    Optimized the popup script and menifest for firefox

--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,30 @@
-import extractYoutubeUrls from "./extractYoutubeUrls.js";
 import copyUrl from "./copyUrl.js";
 
 let urlsContainer = document.getElementById('urlsContainer');
 
 document.getElementById('extractBtn').addEventListener('click', async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab] = await browser.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript(
-      {
-          target: { tabId: tab.id },
-          function: extractYoutubeUrls,
-      },
-      (results) => {
+  browser.tabs.executeScript(tab.id, {
+    file: "./extractYoutubeUrls.js"
+  }, (results) => {
+    const extractedUrls = results && results[0] && results[0].result;
 
-          const extractedUrls = results && results[0] && results[0].result;
+    if (extractedUrls && extractedUrls.length > 0) {
+      const listItems = extractedUrls.map(url => `
+        <li>
+          <div class="container">
+            <a href="${url}">${url}</a>
+            <img class="copyBtn" src="assets/copy.png"/>
+          </div>
+        </li>
+      `).join('');
 
-          if (extractedUrls && extractedUrls.length > 0) {
-              const listItems = extractedUrls.map(url => `
-                  <li>
-                      <div class="container">
-                          <a href="${url}">${url}</a>
-                          <img class="copyBtn" src="assets/copy.png"/>
-                      </div>
-                  </li>
-              `).join('');
-
-              urlsContainer.innerHTML = `<ol>${listItems}</ol>`;
-          } else {
-              urlsContainer.innerHTML = '<p>No Youtube videos found on this page.</p>';
-          }
-      }
-  );
+      urlsContainer.innerHTML = `<ol>${listItems}</ol>`;
+    } else {
+      urlsContainer.innerHTML = '<p>No YouTube videos found on this page.</p>';
+    }
+  });
 });
 
 
@@ -38,8 +32,4 @@ urlsContainer.addEventListener('click', (event) => {
   if (event.target && event.target.classList.contains('copyBtn')) {
     copyUrl(event.target);
   }
-})
-
-
-
-
+});
